566341803bbdde7a0745056710d2889682f9401b
Parth Vasani <67818543+Parth699@users.noreply.github.com>
2022-08-09 17:16:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function changeNavbar(){
 
 // document.querySelector(".app__navbar-menu").addEventListener("onclick",toggleMenu);
 
-function toggleMenu(){
-    console.log("hello")
-    // console.log(document.getElementsByClassName("app__navbar-menu-list").style.height);
-    document.getElementsByClassName("app__navbar-menu-list").style.display=flex;
+function openMenu(){
+    document.querySelector('.app__navbar-menu-list').style.transform='translateX(0%)';
+}
+function closeMenu(){
+    document.querySelector('.app__navbar-menu-list').style.transform='translateX(-100%)';
 }
